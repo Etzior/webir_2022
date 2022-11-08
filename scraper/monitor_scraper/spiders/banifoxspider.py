2e2108bcf8829e64fca38b2f342d74f9f52a0be7
Andrés Collares <andres.collares@fing.edu.uy>
2022-11-08 14:48:48 -0300

    (Scraper) +stock

--- scraper/monitor_scraper/spiders/banifoxspider.py
+++ scraper/monitor_scraper/spiders/banifoxspider.py
@@ -10,14 +10,28 @@ class BanifoxspiderSpider(scrapy.Spider):
     def parse(self, response: TextResponse):
         monitors = response.css("figcaption")
         for monitor in monitors:
-            yield {
-                "name": monitor.css("a.color-secundario.link-primario::text").get(),
-                "price": monitor.css("h4.texto-xl.color-destacado.di.valign-medio::text").get().replace("\n                USD","").replace(" ",""),
-                "url": monitor.css("a").attrib["href"]
-            }
+            relative_url = monitor.css("a").attrib["href"]
+            yield response.follow(
+                relative_url,
+                callback=self.parse_stock,
+                cb_kwargs={
+                    "name": monitor.css("a.color-secundario.link-primario::text").get(),
+                    "price": monitor.css("h4.texto-xl.color-destacado.di.valign-medio::text").get().replace("\n                USD","").replace(" ",""),
+                    "url": relative_url
+                }
+            )
 
         next_page = response.xpath('//div[@class="der paginacion-flechas nofloat-xs di"]/../../@href').get()
         
         if next_page is not None:
             next_page_url = "https://www.banifox.com" + next_page
             yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_stock(self, response, name, price, url):
+        summary = response.css("div.col-5.col-sm-12.text-center-sm.padding-izq.padding-xs-izq-sm")
+        yield {
+                "name": name,
+                "price": price,
+                "url": url,
+                "stock": len(summary.css("div.alert.alert-stock")) == 0
+            }
--- scraper/monitor_scraper/spiders/laacaspider.py
+++ scraper/monitor_scraper/spiders/laacaspider.py
@@ -9,13 +9,27 @@ class LaacaspiderSpider(scrapy.Spider):
     def parse(self, response):
         monitors = response.css("div.product-block")
         for monitor in monitors:
-            yield {
-                "name": monitor.css("h3.name a::text").get(),
-                "price": monitor.css("ins bdi::text").get() or monitor.css("bdi::text").get(),
-                "url": monitor.css("h3.name a").attrib["href"].replace("https://laaca.com.uy","")
-            }
+            relative_url = monitor.css("h3.name a").attrib["href"].replace("https://laaca.com.uy","")
+            yield response.follow(
+                relative_url, 
+                callback=self.parse_stock,
+                cb_kwargs={
+                    "name": monitor.css("h3.name a::text").get(),
+                    "price": monitor.css("ins bdi::text").get() or monitor.css("bdi::text").get(),
+                    "url": relative_url
+                }
+                ) 
 
         next_page_url = response.css('a.next.page-numbers').attrib["href"]
 
         if next_page_url is not None:
             yield response.follow(next_page_url, callback=self.parse)
+
+    def parse_stock(self, response, name, price, url):
+        summary = response.css("div.summary.entry-summary")
+        yield {
+                "name": name,
+                "price": price,
+                "url": url,
+                "stock": len(summary.css("p.stock").xpath("@class").re("in-stock")) > 0
+            }
