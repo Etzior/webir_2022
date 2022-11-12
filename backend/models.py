3b971ffe271c0d509a3523459265a64e312c2379
Agustín Bologna <71099874+Etzior@users.noreply.github.com>
2022-11-12 11:07:37 -0300

    import de monitores (#2)

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,14 @@ from tortoise.contrib.postgres.indexes import GinIndex
 
 class Monitor(Model):
     name = fields.CharField(max_length=255)
+    brand = fields.CharField(max_length=255)
+    size =fields.CharField(max_length=50)
+    panel = fields.CharField(max_length=25)
+    refresh_rate = fields.CharField(max_length=25)
+    min_response_time =fields.CharField(max_length=25)
+    screen_aspect_ratio = fields.CharField(max_length=25)
+    screen_resolution = fields.CharField(max_length=25)
+    url = fields.CharField(max_length=255)
     # TODO: Fill other monitor characteristics
 
     def __str__(self):
